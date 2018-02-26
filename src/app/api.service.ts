cf7227566ef6b80f58c305e3ad12cb6818ebd5ed
VukasinV <vranic.vukasin@gmail.com>
2018-02-26 21:55:06 +0100

    Initial commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export class ApiService {
+
+    private selectedQuestion = new Subject<any>();
+    questionSelected = this.selectedQuestion.asObservable();
+
+    private selectedQuiz = new Subject<any>();
+    quizSelected = this.selectedQuiz.asObservable();
+
+    constructor(private http: HttpClient) { }
+
+    getQuestions(quizId) {
+        return this.http.get(`http://localhost:53474/api/Questions${quizId}`);
+    }
+
+    getQuizzes() {
+        return this.http.get('http://localhost:53474/api/quizzes');
+    }
+
+    getAllQuizzes() {
+        return this.http.get('http://localhost:53474/api/quizzes/all');
+    }
+    
+    postQuestion(question) {
+        this.http.post('http://localhost:53474/api/Questions', question).subscribe(res => {
+            console.log(res);
+        });
+    }
+
+    putQuestion(question) {
+        this.http.put(`http://localhost:53474/api/Questions/${question.id}`, question).subscribe(res => {
+            console.log(res);
+        });
+    }
+
+    postQuiz(quiz) {
+        this.http.post('http://localhost:53474/api/quizzes', quiz).subscribe(res => {
+            console.log(res);
+        });
+    }
+
+        putQuiz(quiz) {
+        this.http.put(`http://localhost:53474/api/Quizzes/${quiz.id}`, quiz).subscribe(res => {
+            console.log(res);
+        });
+    }
+
+    selectQuestion (question){
+        this.selectedQuestion.next(question);
+    }
+
+    selectQuiz (quiz){
+        this.selectedQuiz.next(quiz);
+    }
+}
